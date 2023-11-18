418405b5b2b0dbd63229cb4c1be7d02b949d8aa7
Christian Farmer <christian.gt.farmer@gmail.com>
2023-11-18 13:10:19 -0600

    Initial commit

---
+++ assets/js/games.js
@@ -0,0 +1,8 @@
+const games = [
+  "catch-the-elves-legacy",
+  "catch-the-ramen",
+  "lemonade-game",
+  "light-orbit-legacy",
+  "picky-uppies",
+  "picky-uppies-simple"
+];
---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+
+const container = document.querySelector('#container');
+
+const baseUrl = 'https://cgtfarmer.github.io';
+
+const main = async () => {
+  games.forEach((game) => insertCard(game));
+};
+
+const insertCard = (game) => {
+  const title = createTitle(game);
+
+  container.insertAdjacentHTML('beforeend', `
+    <a href="${baseUrl + game}" class="text-decoration-none">
+      <div class="card" style="width: 18rem; height: 14rem;">
+        <img
+          src="assets/image/${game}.png"
+          class="card-img-top game-logo"
+          style="max-height: 10rem;"
+          onerror="this.src = 'assets/image/blank.png';"
+        />
+
+        <div class="card-body text-center">
+          <h5 class="card-title">${title}</h5>
+        </div>
+      </div>
+    </a>
+  `);
+}
+
+// const fetchGames = async () => {
+//   const response = await fetch('assets/list.json', { method: 'GET' });
+
+//   return await response.json();
+// };
+
+const createTitle = (name) => {
+  return toTitleCase(name.replaceAll('-', ' '))
+}
+
+const toTitleCase = (str) => {
+  return str.toLowerCase()
+    .split(' ')
+    .map((word) => (word.charAt(0).toUpperCase() + word.slice(1)))
+    .join(' ');
+};
+
+main();
